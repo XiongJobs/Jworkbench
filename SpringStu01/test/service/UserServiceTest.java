e31f462deec30bbe94143b5d64f1d9ded3c1679b
W Xiong <1324406183@qq.com>
2018-10-08 09:38:30 +0800

    spring Stu

---
+++ SpringStu01/test/service/UserServiceTest.java
@@ -0,0 +1,25 @@
+package service;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bean.User;
+
+public class UserServiceTest {
+
+	@Test
+	public void testAdd() throws Exception {
+		ApplicationContext  ctx = new ClassPathXmlApplicationContext("configer/beans.xml");
+		
+		
+		UserService service = (UserService)ctx.getBean("userService");
+		
+		
+		User u = new User();
+		u.setUsername("zhangsan");
+		u.setPassword("zhangsan");
+		service.add(u);
+	}
+
+}
